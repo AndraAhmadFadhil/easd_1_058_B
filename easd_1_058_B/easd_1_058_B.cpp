262eff993f8b289a0e092634869e5034ec0c2eea
AndraAhmadFadhil <andraahmadfadil@gmail.com>
2023-04-07 10:50:36 +0700

    output ke layar

--- easd_1_058_B/easd_1_058_B.cpp
+++ easd_1_058_B/easd_1_058_B.cpp
@@ -7,3 +7,16 @@ int mov_count = 0;
 int n;
 
 void input() {
+	while (true)
+	{
+		cout << "Masukkan panjang element array :";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimum panjang array adalah 0" << endl;
+	}
+
+	cout << "\n------------------" << endl;
+	
